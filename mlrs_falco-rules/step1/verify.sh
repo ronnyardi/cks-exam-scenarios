9e63a44666796fe34695cd0e1b2c06263f2e16e4
Ronny Ardi <ronnyardianto29@gmail.com>
2025-11-10 12:11:38 +0700

    fix: verify script

--- mlrs_falco-rules/step1/verify.sh
+++ mlrs_falco-rules/step1/verify.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
 
-kubectl run verify-privileged --image=busybox --overrides='
-{
-  "apiVersion": "v1",
-  "spec": {
-    "containers": [
-      {
-        "name": "verify-privileged",
-        "image": "busybox",
-        "securityContext": {
-          "privileged": true
-        },
-        "command": ["/bin/sh", "-c", "sleep 300"]
-      }
-    ],
-    "restartPolicy": "Never"
-  }
-}'
-
-while true; do kubectl get pod verify-privileged -o jsonpath='{.status.phase}' | grep -q 'Running' && break; sleep 1; done
-
-FALCO_LOGS=$(cat /var/log/syslog | grep 'Warning' | grep '\[P2\] Privileged Container Detected' | grep -m1 'verify-privileged')
+FALCO_LOGS=$(cat /var/log/syslog | grep 'Warning' | grep -m1 '\[P2\] Privileged Container Detected')
 EVT_UTC=$(echo "$FALCO_LOGS" | grep -oP 'evt_time=\K[0-9T:\-+.Z]+')
 
 if [[ -n "$FALCO_LOGS" && -n "$EVT_UTC" ]]; then
   exit 0
 fi
   exit 1
-
-kubectl delete pod verify-privileged --force --grace-period=0
